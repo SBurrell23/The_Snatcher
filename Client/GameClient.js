0be7eef05e0d700176e11cf31c92b494bb9cd821
Steven Burrell <sburrell23@gmail.com>
2023-12-28 21:50:56 -0700

    BASE WORKING CLIENT/SEVER NO EXTRA STUFF YET

---
+++ Client/GameClient.js
@@ -0,0 +1,134 @@
+var socket = null;
+var reConnectInterval = null;
+
+function connectWebSocket() {
+    console.log("Attempting to connect to server...");
+    globalState = null;
+    playerId = -1;
+    
+    clearInterval(reConnectInterval);
+
+    //wss://thesnatcher.onrender.com
+    //ws://localhost:8080
+    socket = new WebSocket('ws://localhost:8080');  
+    socket.addEventListener('open', function () {
+        console.log('Server connection established!');
+        $("#offlineMessage").css("display", "none");
+        socket.addEventListener('message', function (event) {
+            recievedServerMessage(event.data);
+        });
+        requestAnimationFrame(gameLoop);
+    });
+
+
+    socket.addEventListener('close', function(event) {
+        console.log('WebSocket connection closed.');
+        $("#offlineMessage").css("display", "flex");
+        reConnectInterval = setInterval(function() {
+            connectWebSocket();
+        }, 1000) //On disconnect, try to reconnect every second
+    });
+}
+connectWebSocket();
+
+var globalState = null;
+var playerId = -1;
+
+function recievedServerMessage(message) {
+    var message = JSON.parse(message);
+
+    if(message.type == "playerId"){
+        playerId = message.id
+        localStorage.setItem("TSPID", playerId);
+    }
+    else if(message.type == "gs"){
+        globalState = message;
+        updateLobby(globalState);
+        updateInput(globalState,playerId);
+    }
+}
+
+function gameLoop(gs) {
+    if (gs)
+        drawGameState(gs);
+    requestAnimationFrame(gameLoop); // schedule next game loop
+}
+
+
+function updateLobby(gs){
+}
+
+function updateInput(gs,id){
+}
+
+function drawGameState(gs) {
+    
+    var ctx = document.getElementById('canvas').getContext('2d');
+
+    drawBackground(ctx);
+
+}   
+
+function drawBackground(ctx) {
+    const squareSize = 10;
+    const numRows = Math.ceil(ctx.canvas.height / squareSize);
+    const numCols = Math.ceil(ctx.canvas.width / squareSize);
+
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+            if ((row + col) % 2 === 0)
+                ctx.fillStyle = '#B8BADC';
+            else 
+                ctx.fillStyle = '#B2B4D3';
+            ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize);
+        }
+    }
+
+    var lineWidth = 3;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, ctx.canvas.width, lineWidth);
+    ctx.fillRect(0, 0, lineWidth, ctx.canvas.height);
+    ctx.fillRect(ctx.canvas.width - lineWidth, 0, lineWidth, ctx.canvas.height);
+    ctx.fillRect(0, ctx.canvas.height - lineWidth, ctx.canvas.width, lineWidth);
+}
+
+
+$(document).keydown(function(e) {
+
+});
+
+$(document).ready(function() {   
+
+    var canvas = document.getElementById('canvas');
+    var parentDiv = canvas.parentNode;
+    canvas.width = parentDiv.offsetWidth - 100;
+
+    $(window).resize(function() {
+        canvas.width = parentDiv.offsetWidth - 100;
+    });
+
+    var TSPID = localStorage.getItem('TSPID');
+    if (TSPID) {
+        playerId = TSPID;
+    }
+
+    $('#joinGameButton').click(function() {
+        var playerName = $('#playerNameInput').val();
+        if(playerName == ""){
+          playerName = "Player #"+ (Math.floor(Math.random() * 100) + 1);
+          $('#playerNameInput').val(playerName);
+        }
+        socket.send(JSON.stringify({
+            type:"playerJoin",
+            name:playerName,
+            id:playerId
+        }));
+    });
+
+    $('#playerNameInput').keypress(function(e) {
+        if (e.which === 13) {
+            $('#joinGameButton').click();
+        }
+    });
+});
+
---
+++ Server/GameServer.js
@@ -0,0 +1,106 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+var gs = {
+    type: "gs",
+    state: '',
+    players:[],
+    map:[],
+    theSnatcher:undefined
+};
+
+var playerObject ={
+    id: -1,
+    name: "",
+    isConnected: false,
+};
+
+var playTime = 0;
+
+var timeouts = [];
+
+var defaultGameState = JSON.parse(JSON.stringify(gs));
+
+const users = new Map();
+
+wss.on('connection', (ws) => {
+    
+    // Send the current game state to the client
+    ws.on('message', (message) => {
+        message = JSON.parse(message);
+
+        if(message.type == "playerJoin"){
+            console.log("Player join request: " + message.id);
+            if(playerConnected(message.id) == false){
+                var newPlayer = JSON.parse(JSON.stringify(playerObject));
+                newPlayer.isConnected = true;
+                newPlayer.name = message.name;
+                newPlayer.id = String(Date.now());
+                gs.players.push(newPlayer);
+                users.set(ws, newPlayer.id);
+
+                console.log("Player joined: " + JSON.stringify(newPlayer));
+                // Send the player's ID back to the client
+                ws.send(JSON.stringify({ type: "playerId", id: newPlayer.id }));
+
+            }else{
+                //Player is already connected
+                console.log("Player already connected: " + message.id);
+            }
+        }
+    });
+
+    //A user has disconnected
+    ws.on('close', function() {
+        console.log('User id ' + users.get(ws) + ' disconnected');
+        const disconnectedUserId = users.get(ws);
+        users.delete(ws);
+
+        // Remove the player from the gs.players array with the id of the disconnected user
+        gs.players = gs.players.filter(player => player.id !== disconnectedUserId);
+        
+        if(users.size === 0) {
+            console.log('All clients disconnected...');
+            clearAllTimeouts();
+            resetGameState();
+        }
+    });
+
+});
+
+function playerConnected(id){
+    for (let i = 0; i < gs.players.length; i++) 
+        if (gs.players[i].id == id) 
+            return true;
+    return false;
+}
+
+function resetGameState(){
+    gs = JSON.parse(JSON.stringify(defaultGameState));
+}
+
+function updatePlayTime(){
+    playTime += 1;
+}
+
+function createTimeout(fn, delay) {
+    let timeoutId = setTimeout(fn, delay);
+    timeouts.push(timeoutId);
+}
+
+function clearAllTimeouts() {
+    for (let timeoutId of timeouts) {
+        clearTimeout(timeoutId);
+    }
+    timeouts = [];
+}
+
+setInterval(function() {
+    updatePlayTime();
+
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN)
+            client.send(JSON.stringify(gs));
+    });
+}, 32);
